7fbada5d1333c851fb7a5280d4874482ebd3c6d0
uratomo <rop.ibuki@gmail.com>
2020-06-26 23:56:17 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <label htmlFor="bar">bar</label>
-        <input
-          type="text"
-          onChange={() => {
-            console.log("コメント出てくる");
-          }}
-        />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "hana", age: 13 },
+  ];
+  const userList = profiles.map((profile) => {
+    return <User name={profile.name} age={profile.age} />;
+  });
 
-// functionで定義する場合。あまり好きじゃない
-// const App = () => {
-//   return <div>hello</div>;
-// };
+  return <div>{userList}</div>;
+};
+
+const User = (props) => {
+  return (
+    <div>
+      Hi,I am {props.name},and {props.age}
+    </div>
+  );
+};
 
 export default App;
