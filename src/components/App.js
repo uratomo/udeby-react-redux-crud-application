f461afaf47d7845e2f69265eb3f795addd7da23d
uratomo <rop.ibuki@gmail.com>
2020-06-28 10:10:15 +0900

    redux-basic complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,45 @@
 import React from "react";
+// storeの関連付けを行う。重要
+import { connect } from "react-redux";
+// actionをimport
+import { increment, decrement } from "../actions";
 
-const App = () => <Counter />;
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   // this.state = { count: 0 }; <= storeに置き換わる
+  // }
 
-class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
+  // reducerによる管理となる
+  // handlePlusButton = () => {
+  //   this.setState({ count: this.state.count + 1 });
+  // };
 
-  handlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
+  // handleMinusButton = () => {
+  //   this.setState({ count: this.state.count - 1 });
+  // };
 
   render() {
+    //  storeからの値を受け取る
+    const props = this.props;
     return (
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>count: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({ value: state.count.value });
+// const mapDispatchToProps = (dispatch) => ({
+//   increment: () => dispatch(increment),
+//   decrement: () => dispatch(decrement),
+// });
+
+const mapDispatchToProps = { increment, decrement };
+
+//  超重要 connect でactionの値をpropsに入れる方法
+// export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
